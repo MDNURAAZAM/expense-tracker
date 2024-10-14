d6022f929ea324192e72b0867b4c87a826c36590
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-10-14 22:28:12 +0600

    add balance statistics

--- src/components/BalaceStatistics/BalanceStatistics.jsx
+++ src/components/BalaceStatistics/BalanceStatistics.jsx
@@ -9,7 +9,7 @@ const BalanceStatistics = ({ netIncome = 0, netExpense = 0 }) => {
           <div className="bg-[#F9FAFB] flex lg:max-w-xs flex-col px-4 py-4">
             <dt className="text-base leading-7 text-gray-600">Balance</dt>
             <dd className="order-first text-xl font-semibold tracking-tight text-gray-700 sm:text-3xl">
-              BDT {balance}
+              BDT <span style={{ color: balance < 0 && "red" }}>{balance}</span>
             </dd>
           </div>
           <div className="bg-[#F9FAFB] flex lg:max-w-xs flex-col px-4 py-4">
