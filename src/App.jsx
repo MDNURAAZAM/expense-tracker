2828d63ea46635e9f7346bbc071916b6764bbcf9
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-10-14 22:24:29 +0600

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import SubmissionForm from "./components/SubmissionForm/SubmissionForm";
+import BalanceStatistics from "./components/BalaceStatistics/BalanceStatistics";
+
+function App() {
+  const [isIncome, setIsIncome] = useState(false);
+  const [incomeList, setIncomeList] = useState([]);
+  const [expenseList, setExpenseList] = useState([]);
+
+  const handleSave = (category, amount, date) => {
+    const entries = {
+      category,
+      amount: parseInt(amount),
+      date,
+    };
+    if (isIncome) {
+      const id =
+        incomeList?.length > 0 ? incomeList[incomeList.length - 1]?.id + 1 : 0;
+      setIncomeList((prev) => [...prev, { id, ...entries }]);
+    } else {
+      const id =
+        expenseList?.length > 0
+          ? expenseList[expenseList.length - 1]?.id + 1
+          : 0;
+      setExpenseList((prev) => [...prev, { id, ...entries }]);
+    }
+  };
+
+  const netIncome = incomeList?.reduce((a, b) => a + b.amount, 0);
+  const netExpense = expenseList?.reduce((a, b) => a + b.amount, 0);
+
+  
+  return (
+    <>
+      <Navbar />
+      <main className="relative mx-auto mt-10 w-full max-w-7xl">
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <SubmissionForm
+            isIncome={isIncome}
+            setIsIncome={setIsIncome}
+            onSave={handleSave}
+          />
+          <div className="lg:col-span-2">
+            <BalanceStatistics netExpense={netExpense} netIncome={netIncome} />
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
