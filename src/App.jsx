cd6931cf45e11d12a5951849b3d59320a2c1eb83
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-10-15 17:04:27 +0600

    add expense and income container

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import SubmissionForm from "./components/SubmissionForm/SubmissionForm";
 import BalanceStatistics from "./components/BalaceStatistics/BalanceStatistics";
+import IncomeContainer from "./components/IncomeContainer/IncomeContainer";
+import ExpenseContainer from "./components/ExpenseContainer/ExpenseContainer";
 
 function App() {
   const [isIncome, setIsIncome] = useState(false);
@@ -15,6 +17,7 @@ function App() {
       amount: parseInt(amount),
       date,
     };
+    console.log(entries);
     if (isIncome) {
       const id =
         incomeList?.length > 0 ? incomeList[incomeList.length - 1]?.id + 1 : 0;
@@ -31,7 +34,6 @@ function App() {
   const netIncome = incomeList?.reduce((a, b) => a + b.amount, 0);
   const netExpense = expenseList?.reduce((a, b) => a + b.amount, 0);
 
-  
   return (
     <>
       <Navbar />
@@ -44,6 +46,10 @@ function App() {
           />
           <div className="lg:col-span-2">
             <BalanceStatistics netExpense={netExpense} netIncome={netIncome} />
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-8">
+              <IncomeContainer />
+              <ExpenseContainer />
+            </div>
           </div>
         </section>
       </main>
